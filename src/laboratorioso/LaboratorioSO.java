9356f581d1505359c550c70fd07265f68f593736
llherrera <95147465+llherrera@users.noreply.github.com>
2022-05-13 13:15:22 -0500

    avance de ui

--- src/laboratorioso/LaboratorioSO.java
+++ src/laboratorioso/LaboratorioSO.java
@@ -3,11 +3,8 @@ package laboratorioso;
 public class LaboratorioSO {
 
     public static void main(String[] args) {
-        
-        
-        
-        
-        
+        UI ui = new UI();
+        ui.setVisible(true);
         
     }    
 }
