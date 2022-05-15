1d1dcfd0a159ce78c69d9df4dc2667908b216a5e
llherrera <95147465+llherrera@users.noreply.github.com>
2022-05-14 21:56:32 -0500

    ....

--- src/laboratorioso/UI.java
+++ src/laboratorioso/UI.java
@@ -2,6 +2,8 @@ package laboratorioso;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import java.awt.*;
+import javax.swing.table.DefaultTableModel;
 
 public class UI extends javax.swing.JFrame {
 
@@ -10,20 +12,16 @@ public class UI extends javax.swing.JFrame {
     }
 
     ArrayList<Integer> dl = new ArrayList<Integer>();
-    ArrayList<Boolean> rw = new ArrayList<Boolean>();
+    ArrayList<Integer> rw = new ArrayList<Integer>();
     String error;
     int pro = 0;
     int marco = 0;
     int so = 0;
-    int[] tablaP;
-
-    void addAL(ArrayList arr, Object o) {
-        arr.add(o);
-    }
+    int[] tablaMemPri;
 
     void reset() {
         dl = new ArrayList<Integer>();
-        rw = new ArrayList<Boolean>();
+        rw = new ArrayList<Integer>();
     }
 
     boolean potencia2(int num) {
@@ -32,12 +30,6 @@ public class UI extends javax.swing.JFrame {
             res = true;
         }
         return res;
-    }
-
-    void soutV(Object[] o) {
-        for (int i = 0; i < o.length; i++) {
-            System.out.println("" + o[i]);
-        }
     }
 
     void soutAL(ArrayList arr) {
@@ -52,11 +44,11 @@ public class UI extends javax.swing.JFrame {
             if (dl <= tamP && dl >= 0) {
                 String rw = arr[1];
                 if (rw.equals("E")) {
-                    addAL(this.dl, dl);
-                    addAL(this.rw, 0);
+                    this.dl.add(dl);
+                    this.rw.add(0);
                 } else if (rw.equals("L")) {
-                    addAL(this.dl, dl);
-                    addAL(this.rw, 1);
+                    this.dl.add(dl);
+                    this.rw.add(1);
                 } else {
                     error = "Ingrse L, para lectura, o E, para escritura";
                     JOptionPane.showMessageDialog(this, error);
@@ -77,26 +69,29 @@ public class UI extends javax.swing.JFrame {
 
     int[] setMarcos(int tamSO, int tamM, int[] lisM) {
         int numMarSO = tamSO / tamM;
-        if (tamSO % tamM != 0) {numMarSO++;}
+        if (tamSO % tamM != 0) {
+            numMarSO++;
+        }
         int numMarcos = numMarSO + lisM.length;
-        int[] tablaPaginas = new int[numMarcos];
-        
-        for (int i = 0; i < tablaPaginas.length; i++) {
-            tablaPaginas[i] = -1;
+        int[] memPrin = new int[numMarcos];
+
+        for (int i = 0; i < memPrin.length; i++) {
+            memPrin[i] = -1;
         }
-        for (int i = 0; i < tablaPaginas.length; i++) {
+        for (int i = 0; i < memPrin.length; i++) {
             int n = -1;
-            if (!existe(i,lisM)) {
+            if (!existe(i, lisM)) {
                 do {
-                    n = (int)(Math.random()*10);
-                } while (existe(n, tablaPaginas));
-                tablaPaginas[i]=n;
-            }    
+                    n = (int) (Math.random() * 10);
+                } while (existe(n, memPrin));
+                memPrin[i] = n;
+            }
         }
-        return tablaPaginas;
+        setTablaMP(memPrin);
+        return memPrin;
     }
 
-    boolean existe(int n, int[] arr){
+    boolean existe(int n, int[] arr) {
         boolean res = false;
         for (int i = 0; i < arr.length; i++) {
             if (n == arr[i]) {
@@ -105,6 +100,17 @@ public class UI extends javax.swing.JFrame {
             }
         }
         return res;
+    }
+
+    void setTablaMP(int[] tabla){
+        DefaultTableModel modeloMP = (DefaultTableModel) TablaMP.getModel();
+        for (int i = 0; i < tabla.length; i++) {
+            if (tabla[i] != -1) {
+                modeloMP.addRow(new Object[]{i,"SO"});
+            }else{
+                modeloMP.addRow(new Object[]{i,""});
+            }
+        }
     }
     
     void ejecutar(int tamM, int tamSO, int[] lismar) {
@@ -121,34 +127,52 @@ public class UI extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         DisTamPro = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
         DisIns = new javax.swing.JTextField();
         addInsBot = new javax.swing.JButton();
         DisMarLibres = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TablaP = new javax.swing.JTable();
+        iteBot = new javax.swing.JButton();
+        autoBot = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        TablaMP = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setMinimumSize(new java.awt.Dimension(1300, 649));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(DisTamMarco, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 40, 64, -1));
 
         jLabel1.setText("Tamaño marcos:");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 45, -1, -1));
+        getContentPane().add(DisTamSO, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 80, 64, -1));
 
         jLabel2.setText("Tamaño SO:");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(34, 83, -1, -1));
 
         DisTamPro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 DisTamProActionPerformed(evt);
             }
         });
+        getContentPane().add(DisTamPro, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 120, 64, -1));
 
         jLabel3.setText("Tamaño proceso:");
-
-        jLabel4.setText("<html> Instrucción<br> (DL : L/E): </html>");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 121, -1, -1));
 
         DisIns.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 DisInsActionPerformed(evt);
             }
         });
+        getContentPane().add(DisIns, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 160, 64, -1));
 
         addInsBot.setText("Añadir");
         addInsBot.addActionListener(new java.awt.event.ActionListener() {
@@ -156,14 +180,18 @@ public class UI extends javax.swing.JFrame {
                 addInsBotActionPerformed(evt);
             }
         });
+        getContentPane().add(addInsBot, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 150, 85, 31));
 
         DisMarLibres.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 DisMarLibresActionPerformed(evt);
             }
         });
+        getContentPane().add(DisMarLibres, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 220, 64, -1));
 
         jLabel6.setText("<html>Lista de marcos <br/> libres, separe <br/> por coma </html>");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(19, 210, -1, -1));
+        jLabel6.getAccessibleContext().setAccessibleName("\"<html>Line1 <br/> Line2 <br/> Line3</html>\"");
 
         jButton1.setText("Empezar programa");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -171,72 +199,76 @@ public class UI extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 300, 210, 77));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel1)
-                                .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING))
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(DisMarLibres, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(DisTamPro, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(DisTamSO, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(DisTamMarco, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(DisIns, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(addInsBot, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(72, 72, 72)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(818, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(42, 42, 42)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(DisTamMarco, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(DisTamSO, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(DisTamPro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(DisIns, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(addInsBot, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(30, 30, 30)
-                        .addComponent(DisMarLibres, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(44, 44, 44)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(279, Short.MAX_VALUE))
-        );
+        jPanel1.setBackground(new java.awt.Color(153, 153, 255));
+        jPanel1.setMinimumSize(new java.awt.Dimension(1300, 620));
+        jPanel1.setPreferredSize(new java.awt.Dimension(1300, 650));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel6.getAccessibleContext().setAccessibleName("\"<html>Line1 <br/> Line2 <br/> Line3</html>\"");
+        TablaP.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "DirFis", "NumPag", "NumMar", "Val_Inv", "BitModi", "SwIn", "SwOut"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(TablaP);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 20, 350, -1));
+
+        iteBot.setText("Iterar");
+        iteBot.setEnabled(false);
+        jPanel1.add(iteBot, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 460, -1, -1));
+
+        autoBot.setText("too fast");
+        autoBot.setEnabled(false);
+        autoBot.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                autoBotActionPerformed(evt);
+            }
+        });
+        jPanel1.add(autoBot, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 500, 80, -1));
+
+        jLabel4.setText("<html> Instrucción<br> (DL : L/E): </html>");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, -1, -1));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/laboratorioso/disco3.png"))); // NOI18N
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(990, 20, -1, -1));
+
+        TablaMP.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "#Marco", "Proceso"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(TablaMP);
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 20, 120, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1300, 650));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void DisTamProActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DisTamProActionPerformed
@@ -288,7 +320,9 @@ public class UI extends javax.swing.JFrame {
                     for (int i = 0; i < lisMar.length; i++) {
                         lismar[i] = Integer.parseInt(lisMar[i]);
                     }
-                    tablaP = setMarcos(tamSO, tamM, lismar);
+                    iteBot.setEnabled(true);
+                    autoBot.setEnabled(true);
+                    tablaMemPri = setMarcos(tamSO, tamM, lismar);
                 }
             }
         } catch (Exception e) {
@@ -296,6 +330,10 @@ public class UI extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, error);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void autoBotActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_autoBotActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_autoBotActionPerformed
 
     /**
      * @param args the command line arguments
@@ -338,12 +376,20 @@ public class UI extends javax.swing.JFrame {
     private javax.swing.JTextField DisTamMarco;
     private javax.swing.JTextField DisTamPro;
     private javax.swing.JTextField DisTamSO;
+    private javax.swing.JTable TablaMP;
+    private javax.swing.JTable TablaP;
     private javax.swing.JButton addInsBot;
+    private javax.swing.JButton autoBot;
+    private javax.swing.JButton iteBot;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
